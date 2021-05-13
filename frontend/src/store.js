07f6100f5bf6ac2e0ba31d60c9c73e1a7125ec71
Muhammad Ahmad Chand <ahmadchand420@gmail.com>
2021-05-14 02:38:13 +0500

    Work on Update User Profile Screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,12 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers.js'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
+import {
+  userDetailsReducer,
+  userLoginReducer,
+  userRegisterReducer,
+  userUpdateProfileReducer,
+} from './reducers/userReducers'
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
@@ -28,6 +33,8 @@ const reducer = combineReducers({
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 })
 
 const store = createStore(
